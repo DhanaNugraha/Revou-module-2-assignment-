a8a00271221283e7de6df26d08052edd73e17cc0
DhanaNugraha <dhananugraha1511@gmail.com>
2024-12-13 11:44:25 +0700

    feat: module 3 assignment part 2

--- src/main.js
+++ src/main.js
@@ -1,30 +1,21 @@
-// import math
-Math.PI;
-
 // Array of Objects for recipes
-const recipe_list = [
-  {
-    title: "Spaghetti Carbonara",
-    instructions:
-      "1. Cook spaghetti according to package instructions. <br/>\
-      2. In a separate pan, cook pancetta until crispy. <br/>\
-      3. In a bowl, whisk eggs and grated cheese. <br/>\
-      4. Combine spaghetti, pancetta, and egg mixture, stirring quickly. <br/>\
-      5. Serve immediately with additional cheese and black pepper.",
-    image: "https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg",
-  },
-  {
-    title: "Miso Chicken Noodle Soup",
-    instructions:
-      "1. In a saucepan over medium high heat, heat water to boiling. <br/>\
-      2. Add udon noodles. Cook until noodles are softened, 4 to 5 minutes. <br/>\
-      3. Add rotisserie chicken and turn heat to low. <br/>\
-      4. Remove 2 to 3 tablespoons water, and stir miso paste into water until smooth. <br/>\
-      5. Mix diluted miso paste back into noodles. Cook for 2 to 3 minutes.  <br/>\
-      6. Ladle soup into bowls and garnish with everything bagel seasoning. ",
-    image: "https://www.themealdb.com/images/media/meals/lx1kkj1593349302.jpg",
-  },
-];
+async function recipeListFetcher() {
+  try {
+    // throw "ERROR"
+    const recipe_list_raw = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+    const recipe_list_InJson = await recipe_list_raw.json();
+    return recipe_list_InJson;
+  }
+  catch {
+    console.log('Error fetching data! Retry or handle the error')
+    alert('Error fetching data! Retry or handle the error')
+  }
+}
+
+
+// run and store result of async function. Take the first meal in the array.
+let recipe_list = await recipeListFetcher()
+recipe_list = recipe_list.meals[0]
 
 // DOMs for recipe title, instruction, and image
 const recipe_title = document.querySelector(".recipe-title");
@@ -33,13 +24,23 @@ const recipe_instruction = document.querySelector(".recipe-instruction");
 
 const recipe_image = document.querySelector(".recipe-image");
 
+
 // Click event listener for random recipe button
 document.getElementById("getRecipeBtn").addEventListener("click", onRandomRecipe);
 
-// random number generator for to call for array of recipes
-let randomRecipeNumber = Math.floor(Math.random() * recipe_list.length);
 
-console.log(randomRecipeNumber);
+// Manipulate recipe instruction to create line breaks
+function recipeInstructionBuilder(recipeInstructions) {
+  // recipe with 2 \n will only need 1 replacement for each \n to get 2 <br\>
+  if (recipeInstructions.includes("\r\n\r\n")) {
+    return recipeInstructions.replaceAll("\n", "<br/>")
+  }
+  // replace one \n with 2 <br/>
+  else {
+    return recipeInstructions.replaceAll("\n", "<br/><br/>")
+  }
+}
+
 
 // Function that gives out a random recipe
 function onRandomRecipe() {
@@ -47,13 +48,15 @@ function onRandomRecipe() {
   document.getElementById("getRecipeBtn").style.display = "none";
 
   // Change title text content
-  recipe_title.textContent = recipe_list[randomRecipeNumber]["title"];
+  recipe_title.textContent = recipe_list.strMeal;
 
   // Change innerHtml content for instructions to add the breaks in the string
-  recipe_instruction.innerHTML = recipe_list[randomRecipeNumber]["instructions"];
+  recipe_instruction.innerHTML = recipeInstructionBuilder(recipe_list.strInstructions);
 
   // Add recipe image and adjust width to 200px
-  recipe_image.src = recipe_list[randomRecipeNumber]["image"];
-
+  recipe_image.src = recipe_list.strMealThumb;
   recipe_image.style.width = "200px";
+
+  // Set body height to 100% to prevent overflow
+  document.getElementById("body").style.height = "100%";
 }
